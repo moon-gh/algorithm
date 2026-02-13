806bb66d99c05c643b7fbafc845280730053d9ee
문건희 <mo_ongh@naver.com>
2026-02-13 18:30:09 +0900

    Initial commit: 가까운매장찾기.py

---
+++ 가까운매장찾기.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+def distance(store1, store2):
+    return sqrt((store1[0] - store2[0]) ** 2 + (store1[1] - store2[1]) ** 2)
+
+def closest_pair(coordinates):
+    pair = [coordinates[0], coordinates[1]]
+
+    for i in range(len(coordinates) -1):
+        for j in range(i+1, len(coordinates)):
+            store1, store2 = coordinates[i], coordinates[j]
+
+            if distance(pair[0], pair[1]) > distance(store1, store2):
+                pair = [store1, store2]
+    return pair
+test_coordinates = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
+print(closest_pair(test_coordinates))
